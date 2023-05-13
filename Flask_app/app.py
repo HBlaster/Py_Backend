cb224e32fb4539e224ef6120dc75c2b2d92084d7
hblaster <alfredo10396.c@gmail.com>
2023-05-13 15:49:51 -0600

    formulario para envio de contacto

---
+++ Flask_app/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flaskcontacts'
+mysql = MySQL(app)
+
+@app.route('/')
+def Index():
+    return render_template('index.html')
+
+@app.route('/add_contact', methods=['POST'])
+def add_contact():
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        print(fullname)
+        print(phone)
+        print(email)
+        return 'Received'
+
+
+@app.route('/edit_contact')
+def edit_contact():
+    return 'Edit contact'
+
+@app.route('/delete_contact')
+def delete_contact():
+    return 'Delete contact'
+
+
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
