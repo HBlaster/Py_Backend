72e36e9a486c303838832f26ed0712611282ed5f
hblaster <alfredo10396.c@gmail.com>
2023-05-14 12:22:50 -0600

    instalacion de flask

---
+++ products_restapi/products.py
@@ -0,0 +1,11 @@
+products = [
+    {"name":"laptop",
+     "price":800,
+     "quantity":4},
+     {"name":"mouse",
+     "price":40,
+     "quantity":10},
+     {"name":"monitor",
+     "price":500,
+     "quantity":3},
+]
